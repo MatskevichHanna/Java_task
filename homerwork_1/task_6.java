e5e23fa56867fdf103b08bb3d30e3196465db1a0
Matskevich_Hanna <hankohanna@mail.ru>
2023-02-18 00:16:26 +0300

    задачa на замену элементов массива

---
+++ homerwork_1/task_6.java
@@ -0,0 +1,18 @@
+// Задать целочисленный массив, состоящий из элементов 0 и 1.
+// Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+package homerwork_1;
+
+public class task_6 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
+        int arrll = arr.length;
+        for (int i = 0; i < arrll; i++) {
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
+        }
+        for (int i = 0; i < arrll; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
