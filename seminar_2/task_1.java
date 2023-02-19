5a95558f69f7b690d24ed627c3603d56106c5f67
Matskevich_Hanna <hankohanna@mail.ru>
2023-02-19 23:44:01 +0300

    задачa на строку длины N

---
+++ seminar_2/task_1.java
@@ -0,0 +1,24 @@
+// Дано четное число N (>0) и символы c1 и c2.
+// Написать метод, который вернет строку длины N, 
+// которая состоит из чередующихся символов c1 и c2, начиная с c1.
+
+package seminar_2;
+
+public class task_1 {
+
+    public static void main(String[] args) {
+        System.out.println(buildString(6, 'a', 'b')); // ababab
+    }
+
+    private static String buildString(int n, char first, char second) {
+        String N = new String();
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                N += first;
+            } else {
+                N += second;
+            }
+        }
+        return null;
+    }
+}
