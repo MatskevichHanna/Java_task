814e815d9d7de0fbacd881b3066ab583f3d06a3b
Matskevich_Hanna <hankohanna@mail.ru>
2023-02-21 23:24:40 +0300

    задачa на создание и сортировку списка

---
+++ seminar_3/task_1.java
@@ -0,0 +1,22 @@
+// Создать список рандомных чисел от 0 до 100(20 чисел). 
+// Отсортировать этот список. Вывести на экран.
+
+package seminar_3;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class task_1 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> myList = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+            myList.add(rnd.nextInt(10));
+        }
+        System.out.println("Список");
+        System.out.println(myList.toString());
+        myList.sort(null);
+        System.out.println("Отсортированный список");
+        System.out.println(myList.toString());
+    }
+}
