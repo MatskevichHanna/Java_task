9571b64404b966a6b6e3c42b5bf8677eb3580349
Matskevich_Hanna <hankohanna@mail.ru>
2023-03-01 00:44:09 +0300

    задачa на удаление встроенной строки

---
+++ seminar_4/task_2.java
@@ -0,0 +1,28 @@
+package seminar_4;
+
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class task_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque<String> queue = new ArrayDeque<String>();
+
+        boolean run = true;
+
+        while(run)
+        {
+            String word = in.nextLine();
+            if (word.toLowerCase().equals("print")) {
+                System.out.println(queue.toString());
+            } else if (word.toLowerCase().equals("revert")) {
+                queue.removeFirst();
+            } else if (word.toLowerCase().equals("exit")) {
+                run = false;
+            } else {
+                queue.addFirst(word);
+            }
+        }in.close();
+    }
+}
