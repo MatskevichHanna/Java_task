a6ae9229c46d6f9094759504212f62ff2a4f8b65
Matskevich_Hanna <hankohanna@mail.ru>
2023-03-11 23:50:29 +0300

    задачa на удаление встроенной строки

---
+++ seminar_4/task_1.java
@@ -0,0 +1,34 @@
+// Создать arrayList и LinkedList и посмотреть что быстрее работает
+
+package seminar_4;
+
+public class task_1 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int size = 20000000;
+        
+        long arrayListTime = 0;
+        long linkedListTime = 0;
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+        arrayList.add(rnd.nextInt(100));
+        }
+        long end = System.currentTimeMillis();
+        arrayListTime = end - start;
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+        linkedList.add(rnd.nextInt(100));
+        }
+        end = System.currentTimeMillis();
+        linkedListTime = end - start;
+        
+        System.out.println("Время заполнения ArrayList: " + arrayListTime);
+        System.out.println("Время заполнения LinkedList: " + linkedListTime);
+        }
+}
+
