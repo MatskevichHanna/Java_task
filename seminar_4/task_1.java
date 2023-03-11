0e0466a34f69ab653c1ab577b9b41fafaf5f6766
Matskevich_Hanna <hankohanna@mail.ru>
2023-03-11 23:53:46 +0300

    задачa на консольное приложение, которое создает и удаляет строки

--- seminar_4/task_1.java
+++ seminar_4/task_1.java
@@ -2,6 +2,10 @@
 
 package seminar_4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
 public class task_1 {
     public static void main(String[] args) {
         Random rnd = new Random();
@@ -15,20 +19,20 @@ public class task_1 {
         
         long start = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
-        arrayList.add(rnd.nextInt(100));
+            arrayList.add(rnd.nextInt(100));
         }
         long end = System.currentTimeMillis();
         arrayListTime = end - start;
         
         start = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
-        linkedList.add(rnd.nextInt(100));
+            linkedList.add(rnd.nextInt(100));
         }
         end = System.currentTimeMillis();
         linkedListTime = end - start;
         
         System.out.println("Время заполнения ArrayList: " + arrayListTime);
         System.out.println("Время заполнения LinkedList: " + linkedListTime);
-        }
+    }
 }
 
